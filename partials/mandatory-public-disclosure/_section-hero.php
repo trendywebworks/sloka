d24527431bfab6847f59591ad2af94de767536ef
trendywebworks <trendywebworks@gmail.com>
2026-05-23 20:33:22 +0530

    new theme

---
+++ partials/mandatory-public-disclosure/_section-hero.php
@@ -0,0 +1,29 @@
+<!-- start hero section -->
+<section id="home" class="p-0 cover-background small-screen3 height-100 z-index-2"
+    style="background-image: url('https://slokawaldorf.in/wp-content/themes/sloka/images/curriculum/High-School-Banner-01.jpg');"
+    title="7-8 high-school students perform on stage dressed in theatrical costumes.">
+    <div class="container-fluid position-relative small-screen3 height-100 Bg-transparent-dark-gray-Full">
+        <div class="slider-typography page-title-medium">
+            <div class="slider-text-middle-main">
+                <div class="container slider-text-bottom l-container">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-9 col-md-12">
+                            <div class="padding-50px-tb wow fadeInUpBig sm-padding-five-lr">
+                                <div class="">
+
+                                    <h1 class="Underline-Sloka text-white-2 alt-font"><span class="rainbow-energetic">
+                                            Mandatory Public Disclosure </span></h1>
+                                </div>
+                            </div>
+                            <div class="CurriculumSubMenu alt-font wow fadeIn" data-wow-delay="0.6s">                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- end hero section -->
+<div class="line-container"></div>
+<!-- start information section -->
