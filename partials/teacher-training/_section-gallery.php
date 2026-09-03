13953101a5888a6b78eef16ed0db097627c547fb
trendywebworks <trendywebworks@gmail.com>
2026-09-03 17:24:25 +0530

    style fi

--- partials/teacher-training/_section-gallery.php
+++ partials/teacher-training/_section-gallery.php
@@ -1,11 +1,15 @@
 <section class="bg-white padding-50px-tb sm-display-none">
-    <div class="container-fluid">
+    <div class="container">
         <div class="row align-items-center">
             <div class="col-xs-12 col-sm-6">
-                <img src="https://slokawaldorf.in/wp-content/uploads/2026/09/teachers-training-2.jpeg" alt="" class="img-responsive">
+				<a href="https://slokawaldorf.in/wp-content/uploads/2026/09/teachers-training-2.jpeg" target="_blank">
+                	<img src="https://slokawaldorf.in/wp-content/uploads/2026/09/teachers-training-2.jpeg" alt="" class="img-responsive">
+				</a>
                 </div>
                 <div class="col-xs-12 col-sm-6">
-                <img src="https://slokawaldorf.in/wp-content/uploads/2026/09/teachers-training-1.jpeg" alt="" class="img-responsive">
+				<a href="https://slokawaldorf.in/wp-content/uploads/2026/09/teachers-training-1.jpeg" target="_blank">
+                	<img src="https://slokawaldorf.in/wp-content/uploads/2026/09/teachers-training-1.jpeg" alt="" class="img-responsive">
+				</a>
             </div>
         </div>
     </div>
